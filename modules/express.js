18d3b958e4889d074159ec698984a5dd5c2a9903
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-09-05 11:00:18 -0300

    express.js updated (ejs added)

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,9 @@ const UserModel = require('../src/models/user.model');
 
 app.use(express.json())
 
+app.set('view engine', 'ejs')
+app.set('views', '../src/views')
+
 app.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`)
     console.log(`Content Type: ${req.headers['content-type']}`)
